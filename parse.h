fce3c585ac08412d36bcfafc1c07b3baae18774b
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-10-12 11:39:27 +0100

    Remove type definitions to own header file

--- parse.h
+++ parse.h
@@ -15,41 +15,6 @@
 #define BEGIN_LIST_MARK		"_BEGIN_LIST_"
 #define END_LIST_MARK			"_END_LIST_"
 
-typedef struct value_t value_t;
-typedef struct content_t content_t;
-
-struct value_t
-{
-	char *value;
-	size_t vlen;
-};
-
-struct content_t
-{
-	char *data;
-	size_t data_len;
-	size_t alloc_len;
-	off_t off;
-};
-
-struct offset_idx
-{
-	off_t off;
-	int idx;
-};
-
-struct article_header
-{
-	value_t *title;
-	value_t *server_name;
-	value_t *server_ipv4;
-	value_t *server_ipv6;
-	value_t *generator;
-	value_t *lastmod;
-	value_t *downloaded;
-	value_t *content_len;
-};
-
 int extract_wiki_article(buf_t *);
 
 #endif /* !defined PARSE_H */
