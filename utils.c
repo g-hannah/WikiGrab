3b8ba3e7d4d2908518f76a9f2ea424a064e5eeb7
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-10-12 11:16:32 +0100

    Move various useful functions to utils.c

---
+++ utils.c
@@ -0,0 +1,75 @@
+#include <assert.h>
+#include <string.h>
+#include "utils.h"
+
+/**
+ * nested_closing_char - return a pointer to the final closing character
+ * for example, the correct '}' char to go with its opening '{'
+ * @whence: pointer to the opening char
+ * @limit: pointer to our search limit
+ * @o: the opening character
+ * @c: the closing character
+ */
+char *
+nested_closing_char(char *whence, char *limit, char o, char c)
+{
+	assert(whence);
+	assert(limit);
+	assert(limit > whence);
+
+	char *search_from;
+	char *cur_pos;
+	char *savep;
+	char *final;
+	int depth = 0;
+
+	search_from = savep = (whence + 1);
+	final = memchr(search_from, c, (limit - search_from));
+
+	if (!final)
+		return NULL;
+
+	while (1)
+	{
+		savep = search_from;
+
+		while (1)
+		{
+			cur_pos = memchr(savep, o, (final - savep));
+
+			if (!cur_pos)
+				break;
+
+			++depth;
+
+			if (cur_pos >= final)
+				savep = cur_pos;
+			else
+				savep = (cur_pos + 1);
+		}
+
+		if (!depth)
+			break;
+
+		search_from = savep = (final + 1);
+
+		while (depth)
+		{
+			assert(limit >= savep);
+			cur_pos = memchr(savep, c, (limit - savep));
+
+			if (!cur_pos)
+				break;
+
+			--depth;
+
+			final = cur_pos;
+			savep = (final + 1);
+		}
+
+		depth = 0;
+	}
+
+	assert(final);
+	return final;
+}
---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H 1
+
+#include <sys/types.h>
+
+char *nested_closing_char(char *, char *, char, char) __nonnull((1,2)) __wur;
+
+#endif /* !defined UTILS_H */
