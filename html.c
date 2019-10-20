954f93b3b9ba5acc0fd57ddf8483c3bcdb6adec6
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-10-20 10:55:46 +0100

    Remove temporary debugging code

--- html.c
+++ html.c
@@ -961,7 +961,6 @@ html_remove_elements_class(buf_t *buf, const char *classname)
 				content_end = buf->buf_tail;
 
 			range = (content_end - left_angle);
-			fprintf(stderr, "REMOVING CONTENT OF CLASS \"%s\"\n\n%.*s\n\n", classname, (int)range, left_angle);
 			buf_collapse(buf, (off_t)(left_angle - buf->buf_head), range);
 			content_end = savep = left_angle;
 		}
